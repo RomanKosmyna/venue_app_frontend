ddf6368c401ad7dfff9da1dc27a5eb4eb97e7052
RomanKosmyna <roma.kosmyna@gmail.com>
2023-11-21 00:52:43 +0200

    Added Providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { router } from './routers/Routers/routers';
 import { RouterProvider } from 'react-router-dom';
+import Providers from './providers/Providers';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>
   </React.StrictMode>,
 )
